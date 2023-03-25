66d6309c41c9039f526e252b75ef8811f887006a
Tushar Sukhwal <tushars7740@gmail.com>
2023-03-25 21:25:31 +0530

    commit

--- src/constants/index.js
+++ src/constants/index.js
@@ -1,92 +1,119 @@
-const about = "Established in 2003 the undergraduate laboratory for 1st year B.Tech students is equipped with several experiments in different branches in Physics. The lab is divided into two sections, one is UG Physics Lab and the other is UG Physics Optics Lab. Laboratory work is a very important part of a course in General Physics. It reinforces the student's understanding of fundamental concepts and principles while, at the same time, helping the student to develop skill in carrying out scientific measurements. It is designed in such a way that the students can understand the basics of Physics through hands on experiments."
+const about =
+  "Established in 2003 the undergraduate laboratory for 1st year B.Tech students is equipped with several experiments in different branches in Physics. The lab is divided into two sections, one is UG Physics Lab and the other is UG Physics Optics Lab. Laboratory work is a very important part of a course in General Physics. It reinforces the student's understanding of fundamental concepts and principles while, at the same time, helping the student to develop skill in carrying out scientific measurements. It is designed in such a way that the students can understand the basics of Physics through hands on experiments.";
 
-const site_title = "UG Physics Laboratory"
-const title_desc = "Department of Physics , LNMIIT Jaipur"
-
+const site_title = "UG Physics Laboratory";
+const title_desc = "Department of Physics , LNMIIT Jaipur";
 
 const experiments = [
-    {
-        id: 1 , 
-        name : ""
-    },
-    {
-        id: 2 , 
-        name : "Acc. due to gravity by bar pendulum"
-    },
-    {
-        id: 3 ,
-        name : "Helmholtz coil"
-    },
-    {
-        id : 4 , 
-        name : "Measurement of band gap of semiconductor"
-    },
-    {
-        id : 5 , 
-        name : "Refractive index of glass with prism"
-    },
-    {
-        id : 6 , 
-        name : "Wavelength of Sodium light with Newton's Rings"
-    },
-    {
-        id : 7 , 
-        name : "Gyroscope"
-    },
-    {
-        id : 8 , 
-        name : "Electromagnetic Induction"
-    },
-    {
-        id : 9 , 
-        name : "Mechanical waves"
-    },
-    {
-        id : 10 , 
-        name : "Fraunhoffer Diffraction"
-    },
-    {
-        id : 11 , 
-        name : "Diffraction grating"
-    }
-]
+  {
+    id: 1,
+    name: "",
+  },
+  {
+    id: 2,
+    name: "Acc. due to gravity by bar pendulum",
+  },
+  {
+    id: 3,
+    name: "Helmholtz coil",
+  },
+  {
+    id: 4,
+    name: "Measurement of band gap of semiconductor",
+  },
+  {
+    id: 5,
+    name: "Refractive index of glass with prism",
+  },
+  {
+    id: 6,
+    name: "Wavelength of Sodium light with Newton's Rings",
+  },
+  {
+    id: 7,
+    name: "Gyroscope",
+  },
+  {
+    id: 8,
+    name: "Electromagnetic Induction",
+  },
+  {
+    id: 9,
+    name: "Mechanical waves",
+  },
+  {
+    id: 10,
+    name: "Fraunhoffer Diffraction",
+  },
+  {
+    id: 11,
+    name: "Diffraction grating",
+  },
+];
 
 const timeTable = [
   {
-    id : 1 , 
-    day : "MON",
-    first : "PG-Lab-3 (M.Sc. 2nd yr)",
-    second : "PG-Lab-1 (Electronics) (M.Sc. 1st yr)"
+    id: 1,
+    day: "MON",
+    first: "PG-Lab-3 (M.Sc. 2nd yr)",
+    second: "PG-Lab-1 (Electronics) (M.Sc. 1st yr)",
   },
   {
-    id : 2,
-    day : "TUE",
-    first : "Physics UG Lab PG-Lab-1 (Electronics) (M.Sc. 1st year)",
-    second : "Physics UG Lab"
+    id: 2,
+    day: "TUE",
+    first: "Physics UG Lab PG-Lab-1 (Electronics) (M.Sc. 1st year)",
+    second: "Physics UG Lab",
   },
   {
-    id : 3,
-    day : "WED",
-    first : "Physics UG Lab",
-    second : "Physics UG Lab"
+    id: 3,
+    day: "WED",
+    first: "Physics UG Lab",
+    second: "Physics UG Lab",
   },
   {
-    id : 4,
-    day : "THU",
-    first : "Physics UG Lab",
-    second : "Physics UG Lab"
+    id: 4,
+    day: "THU",
+    first: "Physics UG Lab",
+    second: "Physics UG Lab",
   },
   {
-    id : 5 ,
-    day : "FRI",
-    first : "",
-    second : "PG-Lab-3 (M.Sc. 2nd year) PG-Lab-1 (Electronics) (M.Sc. 1st year)"
+    id: 5,
+    day: "FRI",
+    first: "",
+    second: "PG-Lab-3 (M.Sc. 2nd year) PG-Lab-1 (Electronics) (M.Sc. 1st year)",
   },
   {
-    id : 6 , 
-    day : "SAT",
-    first : "PG-Lab-3 (M.Sc. 2nd year)",
-    second : "PG-Lab-3 (M.Sc. 2nd year)"
-  }
-]
+    id: 6,
+    day: "SAT",
+    first: "PG-Lab-3 (M.Sc. 2nd year)",
+    second: "PG-Lab-3 (M.Sc. 2nd year)",
+  },
+];
 
+const quickLink = [
+  "UG Physics Lab Manual",
+  "Research Computational Facilities",
+  "Faculty / Staff",
+  "Institute Brochure '19-20",
+];
+
+const UsefulLinks = [
+  "Home",
+  "Experiment Videos",
+  "Ug Physics Lab Manual",
+  "Contact Us",
+];
+
+const Faculty = [
+  {
+    id: 1,
+    name: "Anupam Singh",
+    description:
+      "Professor & Dean(Student Affairs) \nPh.D.,Carnegie Mellon University,USA  \nResearch Areas:High Energy Physics and Cosmology",
+    photo : "./faculty/1.jpg"
+  },
+  {
+    id : 2, 
+    name : 
+  }
+];
