26c34960a9615a4e919df454f6be0d5d35f806dd
Tushar Sukhwal <tushars7740@gmail.com>
2023-03-25 15:08:05 +0530

    commit

--- src/constants/index.js
+++ src/constants/index.js
@@ -2,3 +2,5 @@
 
 const about = "Established in 2003 the undergraduate laboratory for 1st year B.Tech students is equipped with several experiments in different branches in Physics. The lab is divided into two sections, one is UG Physics Lab and the other is UG Physics Optics Lab. Laboratory work is a very important part of a course in General Physics. It reinforces the student's understanding of fundamental concepts and principles while, at the same time, helping the student to develop skill in carrying out scientific measurements. It is designed in such a way that the students can understand the basics of Physics through hands on experiments."
 
+const site_title = "UG Physics Laboratory"
+const title_desc = "Department of Physics , LNMIIT Jaipur"
