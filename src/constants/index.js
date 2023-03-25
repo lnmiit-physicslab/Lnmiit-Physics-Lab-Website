027c4b75e5cd74afd8b58b307fbbfcab92508134
Tushar Sukhwal <tushars7740@gmail.com>
2023-03-25 16:36:20 +0530

    commit

--- src/constants/index.js
+++ src/constants/index.js
@@ -4,3 +4,53 @@ const about = "Established in 2003 the undergraduate laboratory for 1st year B.T
 
 const site_title = "UG Physics Laboratory"
 const title_desc = "Department of Physics , LNMIIT Jaipur"
+
+
+const experiments = [
+    {
+        id: 1 , 
+        name : ""
+    },
+    {
+        id: 2 , 
+        name : "Acc. due to gravity by bar pendulum"
+    },
+    {
+        id: 3 ,
+        name : "Helmholtz coil"
+    },
+    {
+        id : 4 , 
+        name : "Measurement of band gap of semiconductor"
+    },
+    {
+        id : 5 , 
+        name : "Refractive index of glass with prism"
+    },
+    {
+        id : 6 , 
+        name : "Wavelength of Sodium light with Newton's Rings"
+    },
+    {
+        id : 7 , 
+        name : "Gyroscope"
+    },
+    {
+        id : 8 , 
+        name : "Electromagnetic Induction"
+    },
+    {
+        id : 9 , 
+        name : "Mechanical waves"
+    },
+    {
+        id : 10 , 
+        name : "Fraunhoffer Diffraction"
+    },
+    {
+        id : 11 , 
+        name : "Diffraction grating"
+    }
+
+]
+
