21bfb4943a366d769d87aa54a643243cddf9c59c
AdityaTheJaat <programmingID1306@gamil.com>
2023-03-26 21:07:47 +0530

    Files Created

---
+++ src/constants/NavBar.js
@@ -0,0 +1,12 @@
+function NavBar(){
+    return(
+        <div>
+            <div><Experiments/></div>
+            <div><Videos/></div>
+            <div><Logo/></div>
+            <div><Faculty/></div>
+            <div><Facilities/></div>
+        </div>
+    )
+}
+export default NavBar;
