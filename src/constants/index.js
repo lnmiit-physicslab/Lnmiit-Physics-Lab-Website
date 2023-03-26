a8cf56a558c14b44a295b20f5b47b3198b640b08
Dhruv Chandak <dhruvchandak5@gmail.com>
2023-03-26 18:59:50 +0530

    Updated Videoes Tab

--- src/constants/index.js
+++ src/constants/index.js
@@ -1,108 +1,108 @@
 const about =
-	"Established in 2003 the undergraduate laboratory for 1st year B.Tech students is equipped with several experiments in different branches in Physics. The lab is divided into two sections, one is UG Physics Lab and the other is UG Physics Optics Lab. Laboratory work is a very important part of a course in General Physics. It reinforces the student's understanding of fundamental concepts and principles while, at the same time, helping the student to develop skill in carrying out scientific measurements. It is designed in such a way that the students can understand the basics of Physics through hands on experiments.";
+  "Established in 2003 the undergraduate laboratory for 1st year B.Tech students is equipped with several experiments in different branches in Physics. The lab is divided into two sections, one is UG Physics Lab and the other is UG Physics Optics Lab. Laboratory work is a very important part of a course in General Physics. It reinforces the student's understanding of fundamental concepts and principles while, at the same time, helping the student to develop skill in carrying out scientific measurements. It is designed in such a way that the students can understand the basics of Physics through hands on experiments.";
 
 const site_title = "UG Physics Laboratory";
 const title_desc = "Department of Physics , LNMIIT Jaipur";
 
 const experiments = [
-	{
-		id: 1,
-		name: "",
-		link: "https://youtu.be/34VvJvlhvq0",
-	},
-	{
-		id: 2,
-		name: "Acc. due to gravity by bar pendulum",
-	},
-	{
-		id: 3,
-		name: "Helmholtz coil",
-	},
-	{
-		id: 4,
-		name: "Measurement of band gap of semiconductor",
-	},
-	{
-		id: 5,
-		name: "Refractive index of glass with prism",
-	},
-	{
-		id: 6,
-		name: "Wavelength of Sodium light with Newton's Rings",
-	},
-	{
-		id: 7,
-		name: "Gyroscope",
-	},
-	{
-		id: 8,
-		name: "Electromagnetic Induction",
-	},
-	{
-		id: 9,
-		name: "Mechanical waves",
-	},
-	{
-		id: 10,
-		name: "Fraunhoffer Diffraction",
-	},
-	{
-		id: 11,
-		name: "Diffraction grating",
-	},
+  {
+    id: 1,
+    name: "",
+    link: "https://youtu.be/34VvJvlhvq0",
+  },
+  {
+    id: 2,
+    name: "Acc. due to gravity by bar pendulum",
+  },
+  {
+    id: 3,
+    name: "Helmholtz coil",
+  },
+  {
+    id: 4,
+    name: "Measurement of band gap of semiconductor",
+  },
+  {
+    id: 5,
+    name: "Refractive index of glass with prism",
+  },
+  {
+    id: 6,
+    name: "Wavelength of Sodium light with Newton's Rings",
+  },
+  {
+    id: 7,
+    name: "Gyroscope",
+  },
+  {
+    id: 8,
+    name: "Electromagnetic Induction",
+  },
+  {
+    id: 9,
+    name: "Mechanical waves",
+  },
+  {
+    id: 10,
+    name: "Fraunhoffer Diffraction",
+  },
+  {
+    id: 11,
+    name: "Diffraction grating",
+  },
 ];
 
 const timeTable = [
-	{
-		id: 1,
-		day: "MON",
-		first: "PG-Lab-3 (M.Sc. 2nd yr)",
-		second: "PG-Lab-1 (Electronics) (M.Sc. 1st yr)",
-	},
-	{
-		id: 2,
-		day: "TUE",
-		first: "Physics UG Lab PG-Lab-1 (Electronics) (M.Sc. 1st year)",
-		second: "Physics UG Lab",
-	},
-	{
-		id: 3,
-		day: "WED",
-		first: "Physics UG Lab",
-		second: "Physics UG Lab",
-	},
-	{
-		id: 4,
-		day: "THU",
-		first: "Physics UG Lab",
-		second: "Physics UG Lab",
-	},
-	{
-		id: 5,
-		day: "FRI",
-		first: "",
-		second: "PG-Lab-3 (M.Sc. 2nd year) PG-Lab-1 (Electronics) (M.Sc. 1st year)",
-	},
-	{
-		id: 6,
-		day: "SAT",
-		first: "PG-Lab-3 (M.Sc. 2nd year)",
-		second: "PG-Lab-3 (M.Sc. 2nd year)",
-	},
+  {
+    id: 1,
+    day: "MON",
+    first: "PG-Lab-3 (M.Sc. 2nd yr)",
+    second: "PG-Lab-1 (Electronics) (M.Sc. 1st yr)",
+  },
+  {
+    id: 2,
+    day: "TUE",
+    first: "Physics UG Lab PG-Lab-1 (Electronics) (M.Sc. 1st year)",
+    second: "Physics UG Lab",
+  },
+  {
+    id: 3,
+    day: "WED",
+    first: "Physics UG Lab",
+    second: "Physics UG Lab",
+  },
+  {
+    id: 4,
+    day: "THU",
+    first: "Physics UG Lab",
+    second: "Physics UG Lab",
+  },
+  {
+    id: 5,
+    day: "FRI",
+    first: "",
+    second: "PG-Lab-3 (M.Sc. 2nd year) PG-Lab-1 (Electronics) (M.Sc. 1st year)",
+  },
+  {
+    id: 6,
+    day: "SAT",
+    first: "PG-Lab-3 (M.Sc. 2nd year)",
+    second: "PG-Lab-3 (M.Sc. 2nd year)",
+  },
 ];
 
 const quickLink = [
-	"UG Physics Lab Manual",
-	"Research Computational Facilities",
-	"Faculty / Staff",
-	"Institute Brochure '19-20",
+  "UG Physics Lab Manual",
+  "Research Computational Facilities",
+  "Faculty / Staff",
+  "Institute Brochure '19-20",
 ];
 
 const UsefulLinks = [
-	"Home",
-	"Experiment Videos",
-	"Ug Physics Lab Manual",
-	"Contact Us",
+  "Home",
+  "Experiment Videos",
+  "Ug Physics Lab Manual",
+  "Contact Us",
 ];
 
 const Faculty = [
@@ -190,8 +190,185 @@ const Faculty = [
     id: 12,
     name: "Laxmi Narayan Sharma",
     post: "Staff",
-    degree:"",
-    research:"",
+    degree: "",
+    research: "",
+  },
+
+];
+const YtLinks = [
+  {
+    id: 1,
+    name: "Graph Basics",
+    teacher: "by - Prof. Amit Neogi",
+    link: "https://youtu.be/AGUKQpg_LGw",
+    heading: "Graph",
+  },
+  {
+    id: 2,
+    name: "Semi-Log Graph Part 1",
+    teacher: "by - Prof. Amit Neogi",
+    link: "https://youtu.be/8sd4or77Xgs",
+    heading: "Graph",
+  },
+
+  {
+    id: 3,
+    name: "Semi-Log Graph Part 2",
+    teacher: "by - Prof. Amit Neogi",
+    link: "https://youtu.be/Xv0uyZWz3E8",
+    heading: "Graph",
+  },
+
+  {
+    id: 4,
+    name: "Mass Spring System",
+    teacher: "by - Prof. Somnath Biswas",
+    link: "https://youtu.be/EF_mhQDTB5w",
+    heading: "Instrument",
+  },
+
+  {
+    id: 5,
+    name: "Resistivity Of A Wire",
+    teacher: "by - Prof. Somnath Biswas",
+    link: "https://youtu.be/IreyQWy_0Zg",
+    heading: "Instrument",
+  },
+
+  {
+    id: 6,
+    name: "Screw Gauge",
+    teacher: "by - Prof. Somnath Biswas",
+    link: "https://youtu.be/XXhI4p7IT6c",
+    heading: "Instrument",
+  },
+
+  {
+    id: 7,
+    name: "Wheatstone Bridge",
+    teacher: "by - Prof. Somnath Biswas",
+    link: "https://youtu.be/0ryE3_NqnKI",
+    heading: "Instrument",
+  },
+
+  {
+    id: 8,
+    name: "Wheatstone Bridge Setup",
+    teacher: "by - Prof. Somnath Biswas",
+    link: "https://youtu.be/F12SP8sJmUA",
+    heading: "Instrument",
+  },
+
+  {
+    id: 9,
+    name: "Multimeter",
+    teacher: "by - Prof. Somnath Biswas",
+    link: "https://youtu.be/sD1iJymORxQ",
+    heading: "Instrument",
+  },
+
+  {
+    id: 10,
+    name: "'g' By Bar Pendulum",
+    teacher: "by - Prof. Anupam Singh",
+    link: "https://youtu.be/34VvJvlhvq0",
+    heading: "Experiment",
+  },
+
+  {
+    id: 11,
+    name: "Helmholtz Coil",
+    teacher: "by - Prof. Subhayan Biswas",
+    link: "https://youtu.be/w90jgVHoI8g",
+    heading: "Experiment",
+  },
+
+  {
+    id: 12,
+    name: "Band Gap of Semiconductor",
+    teacher: "by - Prof. Subhayan Biswas",
+    link: "https://youtu.be/YM1Ohh-FM7Q",
+    heading: "Experiment",
+  },
+
+  {
+    id: 13,
+    name: "Band Gap of Semiconductor",
+    teacher: "by - Prof. Subhayan Biswas",
+    link: "https://youtu.be/rJunKtwhYUI",
+    heading: "Experiment",
+  },
+
+  {
+    id: 14,
+    name: "Refractive Index of Glass with Prism",
+    teacher: "by - Prof. Anjishnu Sarkar",
+    link: "https://youtu.be/tnWoL2XiWco",
+    heading: "Experiment",
+  },
+
+  {
+    id: 15,
+    name: "Refractive Index of Glass with Prism",
+    teacher: "by - Prof. Anjishnu Sarkar",
+    link: "https://youtu.be/kQMNaP7X_Tg",
+    heading: "Experiment",
+  },
+
+  {
+    id: 16,
+    name: "Refractive Index of Glass with Prism",
+    teacher: "by - Prof. Anjishnu Sarkar",
+    link: "https://youtu.be/6lSA7v1htfg",
+    heading: "Experiment",
+  },
+
+  {
+    id: 17,
+    name: "Mechanical Waves",
+    teacher: "by - Prof. Amit Neogi",
+    link: "https://youtu.be/v16gm2unMoo",
+    heading: "Experiment",
+  },
+
+  {
+    id: 18,
+    name: "Mechanical Waves",
+    teacher: "by - Prof. Amit Neogi",
+    link: "https://youtu.be/Y4ZuKgz_ui8",
+    heading: "Experiment",
+  },
+
+  {
+    id: 19,
+    name: "Diffraction-Single Slit",
+    teacher: "by- Prof. Boyd F. Edwards",
+    link: "https://youtu.be/uohd0TtqOaw",
+    heading: "Experiment",
+  },
+
+  {
+    id: 20,
+    name: "Fraunhoffer Diffraction",
+    teacher: "by- Shaoul Ezekiel",
+    link: "https://youtu.be/PgW7qaOZD0U",
+    heading: "Experiment",
+  },
+
+  {
+    id: 21,
+    name: "Spectrometer Grating",
+    teacher: "by- Dr. P. Balraju",
+    link: "https://youtu.be/N0lxwqANsd4",
+    heading: "Experiment",
+  },
+
+  {
+    id: 22,
+    name: "Diffraction Grating",
+    teacher: "by- @vt.physics",
+    link: "https://youtu.be/desLn3tMLcc",
+    heading: "Experiment",
   },
 
 ];
