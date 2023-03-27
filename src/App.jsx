917e8399809913fc1e19dd81c75a979bebfb58ba
AdityaTheJaat <programmingID1306@gamil.com>
2023-03-27 11:49:39 +0530

    Footer Added

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,23 @@
 import './App.css'
-import NavBar from './components/NavBar'
 import Footer from './components/Footer'
+import HeaderC from './components/HeaderC'
+import Content from './components/Content'
 function App() {
   return (
     <div className="app relative">
       <div id='doubt' className="w-full h-full flex-col justify-center content-center flex-wrap gap-4 absolute z-10">
-        <div className='w-[100vw] h-[7vh]'><NavBar/></div>
-        <div><img src="src/components/Titlelogo.png" alt="img" className="w-68 h-40 m-auto select-none"></img></div>
-        <div className='text-center select-none'>
-          <p className='text-4xl'>UG Physics Laboratory</p>
-          <p className='text-2xl'>LNMIIT , Jaipur</p>
-        </div>
+        {/* Header Started*/}
+        <HeaderC/>
+        {/* Header Closed */}
+
+        {/* Content Started */}
+        <Content/>
+        {/* Content Closed */}
+
+        {/* Footer Started */}
         <div><Footer/></div>
+        {/* Footer Closed */}
+
       </div>
       <div className='absolute z-0'>
         <img src='src/resources/images/background.png'></img>
--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,8 +2,61 @@ import React from 'react'
 
 const Footer = () => {
     return (
-        <div>
-            
+        <div className='flex content-center justify-center gap-48 p-5 bg-gray-400 bottom-0 w-full h-40'>
+
+            {/* Logo Container Started */}
+            <div className='logo h-28'>
+                <a href="https://www.lnmiit.ac.in/">
+                    <img src="src/resources/images/lnmiit_logo1_reduced.png" alt="LNMIIT Logo" className='w-[12rem] h-[5rem] p-1'></img>    
+                </a>
+            </div>
+            {/* Logo Container Closed */}
+
+            {/* Social Handles Started */}
+            <div className='socialHandles h-28'>
+                <div><p className='text-white text-center'>FOLLOW US</p></div>
+                <div className='flex gap-5'>
+                    <span>
+                        <a href="https://www.facebook.com/lnmiit.ac.in/">
+                            <img src='src/resources/images/socialHandles/facebook.svg' className='w-10'></img>
+                        </a>
+                    </span>
+                    <span>
+                        <a href="https://twitter.com/lnmiit_jaipur?lang=en">
+                            <img src='src/resources/images/socialHandles/twitter.svg' className='w-10'></img>
+                        </a>
+                    </span>
+                    <span>
+                        <a href="https://www.linkedin.com/school/the-lnm-institute-of-information-technology/">
+                            <img src='src/resources/images/socialHandles/linkedin.svg' className='w-10'></img>
+                        </a>
+                    </span>
+                    <span>
+                        <a href="https://www.youtube.com/channel/UCpC73hmoYHg55uyCz2Lti_w">
+                            <img src='src/resources/images/socialHandles/youtube.svg' className='w-10'></img>
+                        </a>
+                    </span>
+                    <span>
+                        <a href="https://www.instagram.com/lnmiit/">
+                            <img src='src/resources/images/socialHandles/instagram.svg' className='w-10'></img>
+                        </a>
+                    </span>
+                </div>
+            </div>
+            {/* Social Handles Closed */}
+
+            {/* Copyright Section Started */}
+            <div className='copyright text-white h-28 w-48 text-xs'>
+                <p>Copyright © 2018-2023 Department of Physics, LNMIIT. All Rights Reserved.</p>
+                <br/>
+                <p>Handcrafted with ❤️  by <br/> Y22 Students <br/>(
+                    <a href='' className='hover:cursor-pointer hover:duration-150 hover:font-bold'>Aditya Chaudhary </a>
+                    <a href='' className='hover:cursor-pointer hover:duration-150 hover:font-bold'>, Tushar Sukhwal </a>
+                    <a href='' className='hover:cursor-pointer hover:duration-150 hover:font-bold'>, Dhruv Chandak)</a>
+                </p>
+            </div>
+            {/* Copyright Section CLosed */}
+
         </div>
     )
 }
