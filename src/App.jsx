9c4552c9f81694dbb46e97082f46ec97a350b4a6
AdityaTheJaat <programmingID1306@gamil.com>
2023-03-27 09:22:36 +0530

    1)NavBar created 2)in App.jsx inside the "doubt" id div flex properties aren't working

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,23 @@
 import './App.css'
 import NavBar from './components/NavBar'
+import Footer from './components/Footer'
 function App() {
   return (
-    <div className="app w-full h-full">
-      <div className='w-[100vw] h-[7vh]'><NavBar/></div>
-      <div></div>
-      <div><Footer/></div>
+    <div className="app relative">
+      <div id='doubt' className="w-full h-full flex-col justify-center content-center flex-wrap gap-4 absolute z-10">
+        <div className='w-[100vw] h-[7vh]'><NavBar/></div>
+        <div><img src="src/components/Titlelogo.png" alt="img" className="w-68 h-40 m-auto select-none"></img></div>
+        <div className='text-center select-none'>
+          <p className='text-4xl'>UG Physics Laboratory</p>
+          <p className='text-2xl'>LNMIIT , Jaipur</p>
+        </div>
+        <div><Footer/></div>
+      </div>
+      <div className='absolute z-0'>
+        <img src='src/resources/images/background.png'></img>
+        <img src='src/resources/images/background.png'></img>
+      </div>
+      
     </div>
   )
 }
--- src/components/Experiments.jsx
+++ src/components/Experiments.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const Experiments = () => {
   return (
     <div>
-      
+      hello
     </div>
   )
 }
