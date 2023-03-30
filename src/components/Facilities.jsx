f566b9dc68507c6cd0c9e343a4052bb78bcc78c0
AdityaTheJaat <programmingID1306@gamil.com>
2023-03-30 10:18:29 +0530

    Facilities page updated

--- src/components/Facilities.jsx
+++ src/components/Facilities.jsx
@@ -1,4 +1,22 @@
 function Facilities() {
-	return <div>this is the facilities page</div>;
+	return (
+		<div>
+			<br/><br/>
+			<p className="font-bold">COMPUTATIONAL RESEARCH FACILITY</p>
+			<p>
+			High Perfomance Computing (HPC) at LNMIIT HPC is a very useful tool in many areas of Science and Engineering Research. There are a large number of researchers across disciplines involved in HPC at LNMIIT Jaipur. The LNMIIT recently acquired High Performance Computing (HPC) machine from HP, to be called henceforth as HPC2014.
+			</p><br/><br/>
+			<p className="font-bold">BRIEF DESCRIPTION :</p>
+			<p className="font-semibold">Hardware :</p>
+			<p>HPC2014 is a machine of 7 nodes - from which 6 nodes are serving as compute nodes and 1 node as master node.The HPC2014 is powered by HP (Hewlett-Packard) having 2 CPU in each node. Each of these CPUs belongs to the latest Intel Xenon (Ivy Bridge) family of CPUs having 10 cores on each processor (total 140 computing cores on 7 Nodes) with a total memory of 672 GB. The HPC2014 is based on Intel Xeon E5-2680V2 2.8 GHz 2 CPU-10-core-IvyBridge on HP-Proliant-SL-230s-Gen8 servers with 96 GB of RAM per node E5-2680vx10 core2.8 GHz.</p>
+			<p className="font-semibold">Software :</p>
+			<p>The HPC2014 operates on RHEL. It has Intel Cluster Studio XE 2013 installed to meet the sophisticated HPC challenges by providing, a comprehensive suite of tools that enables users to boost HPC application performance and reliability. It has the latest Intel FORTRAN, C, C++, MPI compilers and libraries, industry leading MPI library and integrated tool suite for HPC development. It comes with high speed QDR infiniband network switches for ultra fast data transfer among processors.</p>
+			<br/><br/>
+			<p className="font-bold">EXPERIMENTAL FACILITY</p>
+			<p>This is an advanced laboratory for research on experimental materials science. This laboratory is equipped with several sophisticated equipments for synthesis and characterization of materials.</p><br/>
+			<p className="font-bold">Physics Lab Manual</p>
+			<a href="https://aashish157.github.io/lnmiit-physics-lab/resources/docs/lab_manual.pdf"><u>click here</u></a><p>to access the lab manual</p><br/>
+		</div>
+	);
 }
 export default Facilities;
