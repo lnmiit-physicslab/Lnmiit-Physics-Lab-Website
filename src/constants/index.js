b8e112408f884a97f0e0d963936ca8670c7d407b
Tushar Sukhwal <tushars7740@gmail.com>
2023-03-31 12:03:39 +0530

    minor commit

--- src/constants/index.js
+++ src/constants/index.js
@@ -1,386 +1,392 @@
 const about =
-  "Established in 2003 the undergraduate laboratory for 1st year B.Tech students is equipped with several experiments in different branches in Physics. The lab is divided into two sections, one is UG Physics Lab and the other is UG Physics Optics Lab. Laboratory work is a very important part of a course in General Physics. It reinforces the student's understanding of fundamental concepts and principles while, at the same time, helping the student to develop skill in carrying out scientific measurements. It is designed in such a way that the students can understand the basics of Physics through hands on experiments.";
+	"Established in 2003 the undergraduate laboratory for 1st year B.Tech students is equipped with several experiments in different branches in Physics. The lab is divided into two sections, one is UG Physics Lab and the other is UG Physics Optics Lab. Laboratory work is a very important part of a course in General Physics. It reinforces the student's understanding of fundamental concepts and principles while, at the same time, helping the student to develop skill in carrying out scientific measurements. It is designed in such a way that the students can understand the basics of Physics through hands on experiments.";
 
 const site_title = "UG Physics Laboratory";
 const title_desc = "Department of Physics , LNMIIT Jaipur";
 
 export const experiments = [
-  {
-    id: 1,
-    name: ".",
-    link: ["34VvJvlhvq0"],
-  },
-  {
-    id: 2,
-    name: "Acc. due to gravity by bar pendulum",
-    link: ["34VvJvlhvq0"]
-  },
-  {
-    id: 3,
-    name: "Helmholtz coil",
-    link: ["w90jgVHoI8g"]
-  },
-  {
-    id: 4,
-    name: "Measurement of band gap of semiconductor",
-    link: ["YM1Ohh-FM7Q","rJunKtwhYUI"]
-    
-  },
-  {
-    id: 5,
-    name: "Refractive index of glass with prism",
-    link: ["tnWoL2XiWco","kQMNaP7X_Tg","6lSA7v1htfg"]
-  },
-  {
-    id: 6,
-    name: "Wavelength of Sodium light with Newton's Rings",
-    link: ["FUb52bdzAOM"]
-  },
-  {
-    id: 7,
-    name: "Gyroscope",
-    link : [""]
-  },
-  {
-    id: 8,
-    name: "Electromagnetic Induction",
-    link : [""]
-  },
-  {
-    id: 9,
-    name: "Mechanical waves",
-    link : ["v16gm2unMoo","Y4ZuKgz_ui8"]
-  },
-  {
-    id: 10,
-    name: "Fraunhoffer Diffraction",
-    link : ["uohd0TtqOaw","PgW7qaOZD0U"]
-  },
-  {
-    id: 11,
-    name: "Diffraction grating",
-    link : ["N0lxwqANsd4","desLn3tMLcc"]
-  },
+	{
+		id: 1,
+		name: ".",
+		link: ["34VvJvlhvq0"],
+	},
+	{
+		id: 2,
+		name: "Acc. due to gravity by bar pendulum",
+		link: ["34VvJvlhvq0"],
+	},
+	{
+		id: 3,
+		name: "Helmholtz coil",
+		link: ["w90jgVHoI8g"],
+	},
+	{
+		id: 4,
+		name: "Measurement of band gap of semiconductor",
+		link: ["YM1Ohh-FM7Q", "rJunKtwhYUI"],
+	},
+	{
+		id: 5,
+		name: "Refractive index of glass with prism",
+		link: ["tnWoL2XiWco", "kQMNaP7X_Tg", "6lSA7v1htfg"],
+	},
+	{
+		id: 6,
+		name: "Wavelength of Sodium light with Newton's Rings",
+		link: ["FUb52bdzAOM"],
+	},
+	{
+		id: 7,
+		name: "Gyroscope",
+		link: [""],
+	},
+	{
+		id: 8,
+		name: "Electromagnetic Induction",
+		link: [""],
+	},
+	{
+		id: 9,
+		name: "Mechanical waves",
+		link: ["v16gm2unMoo", "Y4ZuKgz_ui8"],
+	},
+	{
+		id: 10,
+		name: "Fraunhoffer Diffraction",
+		link: ["uohd0TtqOaw", "PgW7qaOZD0U"],
+	},
+	{
+		id: 11,
+		name: "Diffraction grating",
+		link: ["N0lxwqANsd4", "desLn3tMLcc"],
+	},
 ];
 
 const timeTable = [
-  {
-    id: 1,
-    day: "MON",
-    first: "PG-Lab-3 (M.Sc. 2nd yr)",
-    second: "PG-Lab-1 (Electronics) (M.Sc. 1st yr)",
-  },
-  {
-    id: 2,
-    day: "TUE",
-    first: "Physics UG Lab PG-Lab-1 (Electronics) (M.Sc. 1st year)",
-    second: "Physics UG Lab",
-  },
-  {
-    id: 3,
-    day: "WED",
-    first: "Physics UG Lab",
-    second: "Physics UG Lab",
-  },
-  {
-    id: 4,
-    day: "THU",
-    first: "Physics UG Lab",
-    second: "Physics UG Lab",
-  },
-  {
-    id: 5,
-    day: "FRI",
-    first: "",
-    second: "PG-Lab-3 (M.Sc. 2nd year) PG-Lab-1 (Electronics) (M.Sc. 1st year)",
-  },
-  {
-    id: 6,
-    day: "SAT",
-    first: "PG-Lab-3 (M.Sc. 2nd year)",
-    second: "PG-Lab-3 (M.Sc. 2nd year)",
-  },
+	{
+		id: 1,
+		day: "MON",
+		first: "PG-Lab-3 (M.Sc. 2nd yr)",
+		second: "PG-Lab-1 (Electronics) (M.Sc. 1st yr)",
+	},
+	{
+		id: 2,
+		day: "TUE",
+		first: "Physics UG Lab PG-Lab-1 (Electronics) (M.Sc. 1st year)",
+		second: "Physics UG Lab",
+	},
+	{
+		id: 3,
+		day: "WED",
+		first: "Physics UG Lab",
+		second: "Physics UG Lab",
+	},
+	{
+		id: 4,
+		day: "THU",
+		first: "Physics UG Lab",
+		second: "Physics UG Lab",
+	},
+	{
+		id: 5,
+		day: "FRI",
+		first: "",
+		second: "PG-Lab-3 (M.Sc. 2nd year) PG-Lab-1 (Electronics) (M.Sc. 1st year)",
+	},
+	{
+		id: 6,
+		day: "SAT",
+		first: "PG-Lab-3 (M.Sc. 2nd year)",
+		second: "PG-Lab-3 (M.Sc. 2nd year)",
+	},
 ];
 
 const quickLink = [
-  "UG Physics Lab Manual",
-  "Research Computational Facilities",
-  "Faculty / Staff",
-  "Institute Brochure '19-20",
+	"UG Physics Lab Manual",
+	"Research Computational Facilities",
+	"Faculty / Staff",
+	"Institute Brochure '19-20",
 ];
 
 const UsefulLinks = [
-  "Home",
-  "Experiment Videos",
-  "Ug Physics Lab Manual",
-  "Contact Us",
+	"Home",
+	"Experiment Videos",
+	"Ug Physics Lab Manual",
+	"Contact Us",
 ];
 
 export const facultyMembers = [
-  {
-    id:1,
-    name: "Anupam Singh",
-    post: "Professor & Dean(Student Affairs)",
-    degree: "Ph.D.,Carnegie Mellon University,USA",
-    research: "High Energy Physics and Cosmology",
-  },
-  {
-    id:2,
-    name: "Ganesh Datt Sharma",
-    post: "Professor & Dean(Research & Consultancy)",
-    degree: "Ph.D.,IIT Delhi",
-    research:
-      "Organic Electronics,Organic Solar Cells, Dye Sensitized Solar Cells,Nano-Science and Technology",
-  },
-  {
-    id: 3,
-    name: "Somnath Biswas",
-    post: "Professor & HOD(Dept. of Physics)",
-    degree: "Ph.D.,IIT Kharagpur",
-    research: "Research Areas:Magnetic Nano-materials and Nano-Structures,Spintronic Devices,FET-based Sensor Devices ,Materials for Fuel Cells and Hydrogen Storage",
-  },
-  {
-    id: 4,
-    name: "Subhayan Biswas",
-    post: "Associate Professor",
-    degree: "Ph.D.,IACS ,Kolkata",
-    research: "Research Areas:Third Generation Solar Cells,Photocatalysis",
-  },
-  {
-    id: 5,
-    name: "Amit Neogi",
-    post: "Assistant Professor & Chief Warden",
-    degree: "Ph.D.,IIT Kanpur",
-    research: "Research Areas:Laser Plasma",
-  },
+	{
+		id: 1,
+		name: "Anupam Singh",
+		post: "Professor & Dean(Student Affairs)",
+		degree: "Ph.D.,Carnegie Mellon University,USA",
+		research: "High Energy Physics and Cosmology",
+	},
+	{
+		id: 2,
+		name: "Ganesh Datt Sharma",
+		post: "Professor & Dean(Research & Consultancy)",
+		degree: "Ph.D.,IIT Delhi",
+		research:
+			"Organic Electronics,Organic Solar Cells, Dye Sensitized Solar Cells,Nano-Science and Technology",
+	},
+	{
+		id: 3,
+		name: "Somnath Biswas",
+		post: "Professor & HOD(Dept. of Physics)",
+		degree: "Ph.D.,IIT Kharagpur",
+		research:
+			"Research Areas:Magnetic Nano-materials and Nano-Structures,Spintronic Devices,FET-based Sensor Devices ,Materials for Fuel Cells and Hydrogen Storage",
+	},
+	{
+		id: 4,
+		name: "Subhayan Biswas",
+		post: "Associate Professor",
+		degree: "Ph.D.,IACS ,Kolkata",
+		research: "Research Areas:Third Generation Solar Cells,Photocatalysis",
+	},
+	{
+		id: 5,
+		name: "Amit Neogi",
+		post: "Assistant Professor & Chief Warden",
+		degree: "Ph.D.,IIT Kanpur",
+		research: "Research Areas:Laser Plasma",
+	},
 
-  {
-    id: 6,
-    name: "Anjishnu Sarkar",
-    post: "Assistant Professor",
-    degree: "Ph.D.,IIT Bombay",
-    research: "Research Areas:Cosmology,Particle Physics,Supersymmetric Model Building",
-  },
-  {
-    id: 7,
-    name: "Ashok Garai",
-    post: "Assistant Professor",
-    degree: "Ph.D.,IIT Kanpur",
-    research: "Research Areas:Theoretical & Computational Biophysics, Molecular Motors,Genetic Networks,Theory of Single Molecular Pulling Experiments,DNA Nanomechanics",
-
-  },
-  {
-    id: 8,
-    name: "Manish Kumar Singh",
-    post: "Assistant Professor",
-    degree: "Ph.D.,MNNIT,Allahabad",
-    research: "Research Areas:Solid State Physics ,Electronics,Nanomaterials,Laser Ablation in Liquids & Biosensors",
-  },
-  {
-    id: 9,
-    name: "Nabyendu Das",
-    post: "Assistant Professor",
-    degree: "Ph.D.,Institute of Physics,Bhubaneshwar",
-    research: "Research Areas:Theoritical Condensed Matter Physics",
-  },
-  {
-    id: 10,
-    name: "Pomita Ghoshal",
-    post: "Assistant Professor",
-    degree: "Ph.D.,HRi,Allahabad University",
-    research: "Research Areas:Particle Physics",
-  },
-  {
-    id: 11,
-    name: "Rakesh Tibrewala",
-    post: "Assistant Professor",
-    degree: "Ph.D.,Tata Institue of Fundamental Research,Mumbai",
-    research: "Research Areas:General Relativity,Aspects of Quantum Gravity",
-  },
-  {
-    id: 12,
-    name: "Laxmi Narayan Sharma",
-    post: "Staff",
-    degree: "",
-    research: "",
-  },
-
+	{
+		id: 6,
+		name: "Anjishnu Sarkar",
+		post: "Assistant Professor",
+		degree: "Ph.D.,IIT Bombay",
+		research:
+			"Research Areas:Cosmology,Particle Physics,Supersymmetric Model Building",
+	},
+	{
+		id: 7,
+		name: "Ashok Garai",
+		post: "Assistant Professor",
+		degree: "Ph.D.,IIT Kanpur",
+		research:
+			"Research Areas:Theoretical & Computational Biophysics, Molecular Motors,Genetic Networks,Theory of Single Molecular Pulling Experiments,DNA Nanomechanics",
+	},
+	{
+		id: 8,
+		name: "Manish Kumar Singh",
+		post: "Assistant Professor",
+		degree: "Ph.D.,MNNIT,Allahabad",
+		research:
+			"Research Areas:Solid State Physics ,Electronics,Nanomaterials,Laser Ablation in Liquids & Biosensors",
+	},
+	{
+		id: 9,
+		name: "Nabyendu Das",
+		post: "Assistant Professor",
+		degree: "Ph.D.,Institute of Physics,Bhubaneshwar",
+		research: "Research Areas:Theoritical Condensed Matter Physics",
+	},
+	{
+		id: 10,
+		name: "Pomita Ghoshal",
+		post: "Assistant Professor",
+		degree: "Ph.D.,HRi,Allahabad University",
+		research: "Research Areas:Particle Physics",
+	},
+	{
+		id: 11,
+		name: "Rakesh Tibrewala",
+		post: "Assistant Professor",
+		degree: "Ph.D.,Tata Institue of Fundamental Research,Mumbai",
+		research: "Research Areas:General Relativity,Aspects of Quantum Gravity",
+	},
+	{
+		id: 12,
+		name: "Laxmi Narayan Sharma",
+		post: "Staff",
+		degree: "",
+		research: "",
+	},
 ];
 export const YtLinks = [
-  {
-    id: 1,
-    name: "Graph Basics",
-    teacher: "by - Prof. Amit Neogi",
-    link: "AGUKQpg_LGw",
-    heading: "Graph",
-  },
-  {
-    id: 2,
-    name: "Semi-Log Graph Part 1",
-    teacher: "by - Prof. Amit Neogi",
-    link: "8sd4or77Xgs",
-    heading: "Graph",
-  },
+	{
+		id: 1,
+		name: "Graph Basics",
+		teacher: "by - Prof. Amit Neogi",
+		link: "AGUKQpg_LGw",
+		heading: "Graph",
+	},
+	{
+		id: 2,
+		name: "Semi-Log Graph Part 1",
+		teacher: "by - Prof. Amit Neogi",
+		link: "8sd4or77Xgs",
+		heading: "Graph",
+	},
 
-  {
-    id: 3,
-    name: "Semi-Log Graph Part 2",
-    teacher: "by - Prof. Amit Neogi",
-    link: "Xv0uyZWz3E8",
-    heading: "Graph",
-  },
+	{
+		id: 3,
+		name: "Semi-Log Graph Part 2",
+		teacher: "by - Prof. Amit Neogi",
+		link: "Xv0uyZWz3E8",
+		heading: "Graph",
+	},
 
-  {
-    id: 4,
-    name: "Mass Spring System",
-    teacher: "by - Prof. Somnath Biswas",
-    link: "EF_mhQDTB5w",
-    heading: "Instrument",
-  },
+	{
+		id: 4,
+		name: "Mass Spring System",
+		teacher: "by - Prof. Somnath Biswas",
+		link: "EF_mhQDTB5w",
+		heading: "Instrument",
+	},
 
-  {
-    id: 5,
-    name: "Resistivity Of A Wire",
-    teacher: "by - Prof. Somnath Biswas",
-    link: "IreyQWy_0Zg",
-    heading: "Instrument",
-  },
+	{
+		id: 5,
+		name: "Resistivity Of A Wire",
+		teacher: "by - Prof. Somnath Biswas",
+		link: "IreyQWy_0Zg",
+		heading: "Instrument",
+	},
 
-  {
-    id: 6,
-    name: "Screw Gauge",
-    teacher: "by - Prof. Somnath Biswas",
-    link: "XXhI4p7IT6c",
-    heading: "Instrument",
-  },
+	{
+		id: 6,
+		name: "Screw Gauge",
+		teacher: "by - Prof. Somnath Biswas",
+		link: "XXhI4p7IT6c",
+		heading: "Instrument",
+	},
 
-  {
-    id: 7,
-    name: "Wheatstone Bridge",
-    teacher: "by - Prof. Somnath Biswas",
-    link: "0ryE3_NqnKI",
-    heading: "Instrument",
-  },
+	{
+		id: 7,
+		name: "Wheatstone Bridge",
+		teacher: "by - Prof. Somnath Biswas",
+		link: "0ryE3_NqnKI",
+		heading: "Instrument",
+	},
 
-  {
-    id: 8,
-    name: "Wheatstone Bridge Setup",
-    teacher: "by - Prof. Somnath Biswas",
-    link: "F12SP8sJmUA",
-    heading: "Instrument",
-  },
+	{
+		id: 8,
+		name: "Wheatstone Bridge Setup",
+		teacher: "by - Prof. Somnath Biswas",
+		link: "F12SP8sJmUA",
+		heading: "Instrument",
+	},
 
-  {
-    id: 9,
-    name: "Multimeter",
-    teacher: "by - Prof. Somnath Biswas",
-    link: "sD1iJymORxQ",
-    heading: "Instrument",
-  },
+	{
+		id: 9,
+		name: "Multimeter",
+		teacher: "by - Prof. Somnath Biswas",
+		link: "sD1iJymORxQ",
+		heading: "Instrument",
+	},
 
-  {
-    id: 10,
-    name: "'g' By Bar Pendulum",
-    teacher: "by - Prof. Anupam Singh",
-    link: "34VvJvlhvq0",
-    heading: "Experiment",
-  },
+	{
+		id: 10,
+		name: "'g' By Bar Pendulum",
+		teacher: "by - Prof. Anupam Singh",
+		link: "34VvJvlhvq0",
+		heading: "Experiment",
+	},
 
-  {
-    id: 11,
-    name: "Helmholtz Coil",
-    teacher: "by - Prof. Subhayan Biswas",
-    link: "w90jgVHoI8g",
-    heading: "Experiment",
-  },
+	{
+		id: 11,
+		name: "Helmholtz Coil",
+		teacher: "by - Prof. Subhayan Biswas",
+		link: "w90jgVHoI8g",
+		heading: "Experiment",
+	},
 
-  {
-    id: 12,
-    name: "Band Gap of Semiconductor",
-    teacher: "by - Prof. Subhayan Biswas",
-    link: "YM1Ohh-FM7Q",
-    heading: "Experiment",
-  },
+	{
+		id: 12,
+		name: "Band Gap of Semiconductor",
+		teacher: "by - Prof. Subhayan Biswas",
+		link: "YM1Ohh-FM7Q",
+		heading: "Experiment",
+	},
 
-  {
-    id: 13,
-    name: "Band Gap of Semiconductor",
-    teacher: "by - Prof. Subhayan Biswas",
-    link: "rJunKtwhYUI",
-    heading: "Experiment",
-  },
+	{
+		id: 13,
+		name: "Band Gap of Semiconductor",
+		teacher: "by - Prof. Subhayan Biswas",
+		link: "rJunKtwhYUI",
+		heading: "Experiment",
+	},
 
-  {
-    id: 14,
-    name: "Refractive Index of Glass with Prism",
-    teacher: "by - Prof. Anjishnu Sarkar",
-    link: "tnWoL2XiWco",
-    heading: "Experiment",
-  },
+	{
+		id: 14,
+		name: "Refractive Index of Glass with Prism",
+		teacher: "by - Prof. Anjishnu Sarkar",
+		link: "tnWoL2XiWco",
+		heading: "Experiment",
+	},
 
-  {
-    id: 15,
-    name: "Refractive Index of Glass with Prism",
-    teacher: "by - Prof. Anjishnu Sarkar",
-    link: "kQMNaP7X_Tg",
-    heading: "Experiment",
-  },
+	{
+		id: 15,
+		name: "Refractive Index of Glass with Prism",
+		teacher: "by - Prof. Anjishnu Sarkar",
+		link: "kQMNaP7X_Tg",
+		heading: "Experiment",
+	},
 
-  {
-    id: 16,
-    name: "Refractive Index of Glass with Prism",
-    teacher: "by - Prof. Anjishnu Sarkar",
-    link: "6lSA7v1htfg",
-    heading: "Experiment",
-  },
+	{
+		id: 16,
+		name: "Refractive Index of Glass with Prism",
+		teacher: "by - Prof. Anjishnu Sarkar",
+		link: "6lSA7v1htfg",
+		heading: "Experiment",
+	},
 
-  {
-    id: 17,
-    name: "Mechanical Waves",
-    teacher: "by - Prof. Amit Neogi",
-    link: "v16gm2unMoo",
-    heading: "Experiment",
-  },
+	{
+		id: 17,
+		name: "Mechanical Waves",
+		teacher: "by - Prof. Amit Neogi",
+		link: "v16gm2unMoo",
+		heading: "Experiment",
+	},
 
-  {
-    id: 18,
-    name: "Mechanical Waves",
-    teacher: "by - Prof. Amit Neogi",
-    link: "Y4ZuKgz_ui8",
-    heading: "Experiment",
-  },
+	{
+		id: 18,
+		name: "Mechanical Waves",
+		teacher: "by - Prof. Amit Neogi",
+		link: "Y4ZuKgz_ui8",
+		heading: "Experiment",
+	},
 
-  {
-    id: 19,
-    name: "Diffraction-Single Slit",
-    teacher: "by- Prof. Boyd F. Edwards",
-    link: "uohd0TtqOaw",
-    heading: "Experiment",
-  },
+	{
+		id: 19,
+		name: "Diffraction-Single Slit",
+		teacher: "by- Prof. Boyd F. Edwards",
+		link: "uohd0TtqOaw",
+		heading: "Experiment",
+	},
 
-  {
-    id: 20,
-    name: "Fraunhoffer Diffraction",
-    teacher: "by- Shaoul Ezekiel",
-    link: "PgW7qaOZD0U",
-    heading: "Experiment",
-  },
+	{
+		id: 20,
+		name: "Fraunhoffer Diffraction",
+		teacher: "by- Shaoul Ezekiel",
+		link: "PgW7qaOZD0U",
+		heading: "Experiment",
+	},
 
-  {
-    id: 21,
-    name: "Spectrometer Grating",
-    teacher: "by- Dr. P. Balraju",
-    link: "N0lxwqANsd4",
-    heading: "Experiment",
-  },
+	{
+		id: 21,
+		name: "Spectrometer Grating",
+		teacher: "by- Dr. P. Balraju",
+		link: "N0lxwqANsd4",
+		heading: "Experiment",
+	},
 
-  {
-    id: 22,
-    name: "Diffraction Grating",
-    teacher: "by- @vt.physics",
-    link: "desLn3tMLcc",
-    heading: "Experiment",
-  },
-
+	{
+		id: 22,
+		name: "Diffraction Grating",
+		teacher: "by- @vt.physics",
+		link: "desLn3tMLcc",
+		heading: "Experiment",
+	},
+	{
+		id: 23,
+		name: "dev",
+		teacher: "dev",
+		link: "S5a0eXPoc",
+		heading: "Experiment",
+	},
 ];
-
