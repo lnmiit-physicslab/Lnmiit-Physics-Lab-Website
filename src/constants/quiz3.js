93f1e4c4bf2e68ec3a42f65b6113163fb48fddb3
AdityaTheJaat <programmingID1306@gamil.com>
2023-05-09 11:56:51 +0530

    quiz created

---
+++ src/constants/quiz3.js
@@ -0,0 +1,52 @@
+export const quiz3 = {
+  topic: 'Javascript',
+  level: 'Beginner',
+  totalQuestions: 6,
+  perQuestionScore: 5,
+  questions: [
+    {
+      question:
+        'The electron(s) velocity after acceleration through the potential V',
+      choices: ['v2=eV⁄2m', 'v2=2eV⁄m', 'v=2eV2⁄m', 'v=eV⁄2m'],
+      type: 'MCQs',
+      correctAnswer: 'v2=2eV⁄m',
+    },
+    {
+      question:
+        'The magnetic force Fm acting on a particle of charge q moving with velocity v in a magnetic field B is',
+      choices: ['Fm=q(vXB)', 'Fm=q(BXv)', 'Fm=qB⁄v', 'Fm=qv⁄B'],
+      type: 'MCQs',
+      correctAnswer: 'Fm=q(vXB)',
+    },
+    {
+      question:
+        'A charged particle will move through region getting undeflected if',
+      choices: [
+        'v = B⁄E',
+        'v = BE',
+        'v = E⁄B',
+        'v=B+E',
+      ],
+      type: 'MCQs',
+      correctAnswer: 'v = E⁄B',
+    },
+    {
+      question: 'How much does the maximum EMF produced by a generator (a rotating coil in a uniform B field) change if the area of the coil is doubled and the angular frequency halved?',
+      choices: ['it is the same', 'it is increased by a factor of two', 'it is increased by a factor of eight', 'it is decreased by a factor of four'],
+      type: 'MCQs',
+      correctAnswer: 'it is the same',
+    },
+    {
+      question: 'The magnetic field at center of the coils with N wire windings is proportional',
+      choices: ['Voltage', 'Current through coils', 'Both a & b', 'None'],
+      type: 'MCQs',
+      correctAnswer: 'Current through coils'
+    },
+    {
+      question: 'Which is the equation of the magnetic field according to Biot-Savart(s) law?',
+      choices: ['IdIsin0/2', '(Mo/4Pi)IdIsin0/r', '(Mo/4Pi)IdIsin0/r2', '(Mo/4Pi) IdIsin0'],
+      type: 'MCQs',
+      correctAnswer: '(Mo/4Pi)IdIsin0/r2'
+    }
+  ],
+}
---
+++ src/constants/quiz5.js
@@ -0,0 +1,64 @@
+export const quiz5 = {
+  topic: 'Javascript',
+  level: 'Beginner',
+  totalQuestions: 8,
+  perQuestionScore: 5,
+  questions: [
+    {
+      question:
+        'Conductivity of semiconductor is roughly in the range of (in siemens per centimeter).',
+      choices: ['10^6 to 10^-8', '10^4 to 10^-6', '10^3 to 10^-6', '10^3 to 10^-8'],
+      type: 'MCQs',
+      correctAnswer: '10^3 to 10^-8',
+    },
+    {
+      question:
+        ' The electrical conductivity of a semiconductor varies with',
+      choices: ['Doping concentration', 'Temperature', 'Carrier injection', 'all of the above'],
+      type: 'MCQs',
+      correctAnswer: 'all of the above)',
+    },
+    {
+      question:
+        'In case of semiconductor, increase of temperature,conductivity',
+      choices: [
+        'Increases',
+        'Decreases',
+        'No effect',
+        'None of the the above',
+      ],
+      type: 'MCQs',
+      correctAnswer: 'Increases',
+    },
+    {
+      question: 'In case of semiconductor and insulator, at temperature 0K all the energy levels up to a certain energy band are called',
+      choices: ['Conduction band', 'Valence band', 'Fundamental energy band gap', 'none'],
+      type: 'MCQs',
+      correctAnswer: 'Valence band',
+    },
+    {
+      question: 'In case of semiconductor, the band gap is',
+      choices: ['0 - 4 eV', '0 eV', '4 - 8 eV', '4 eV'],
+      type: 'MCQs',
+      correctAnswer: '0 - 4 eV'
+    },
+    {
+      question: 'Which important characteristic property of semiconductor dictates it’s electrical, optical and optoelectrical properties',
+      choices: ['Valence band', 'Conduction band', 'Fundamental energy band gap', 'none'],
+      type: 'MCQs',
+      correctAnswer: 'Fundamental energy band gap'
+    },
+    {
+      question: 'The conductivity of a semiconductor is given by where μp and μn are the mobilities of holes and electrons',
+      choices: ['(μnn+μpp)', 'e(μnn+μpp)', 'e(μnp+μpn)', '(μnp+μpn)'],
+      type: 'MCQs',
+      correctAnswer: 'e(μnn+μpp)'
+    },
+    {
+      question: 'Which of the following statements is not true?',
+      choices: ['The resistance of intrinsic semiconductor decreases with increase of temperature', 'Doping pure Si with trivalent impurities gives p-type semiconductors', 'The majority carriers in ntype semiconductors are hole', 'A p-n junction can act as asemiconductor diode'],
+      type: 'MCQs',
+      correctAnswer: 'The majority carriers in ntype semiconductors are hole'
+    }
+  ],
+}
