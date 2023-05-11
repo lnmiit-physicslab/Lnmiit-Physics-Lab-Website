4c1485d76323fa8898126fbae9b26780542db473
AdityaTheJaat <programmingID1306@gamil.com>
2023-05-12 01:47:05 +0530

    Site updated

--- src/components/AnimatedLogo.jsx
+++ src/components/AnimatedLogo.jsx
@@ -16,9 +16,9 @@ const AnimatedLogo = () => {
 					<div class="electron"></div>
 				</div>
 			</div>
-			<div className="select-none text-center  pt-20 md:pt-0">
-				<p className="hello-2 text-[80px]">UG Physics Laboratory</p>
-				<p className="text-2xl -z-50 hello">LNMIIT , Jaipur</p>
+			<div className="select-none text-center pt-20 md:pt-0">
+				<p className="hello-2 text-[90px]">UG Physics Laboratory</p>
+				<p className="text-3xl -z-50 hello">LNMIIT , Jaipur</p>
 			</div>
 		</div>
 	);
